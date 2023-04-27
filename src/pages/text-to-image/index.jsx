bcdfd9e4ef167308eddd69db694e1f6c85fcae6e
anas derk <anas.derk2020@gmail.com>
2023-04-27 07:47:14 -0700

    Change Local Host To IP

--- src/pages/text-to-image/index.jsx
+++ src/pages/text-to-image/index.jsx
@@ -20,7 +20,7 @@ const TextToImage = () => {
     const textToImageGenerate = (e) => {
         e.preventDefault();
         setIsWaitStatus(true);
-        Axios.get(`http://localhost:4000/api/text-to-image-generate?textPrompt=${textPrompt}`)
+        Axios.get(`http://127.0.0.1:4000/api/text-to-image-generate?textPrompt=${textPrompt}`)
         .then((res) => {
             let imageURLs = res.data;
             setIsWaitStatus(false);
