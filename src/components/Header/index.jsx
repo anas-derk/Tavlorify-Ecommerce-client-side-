eb8c16ac8827c59d51dba80b95c96b80006de8ed
anas derk <anas.derk2020@gmail.com>
2023-06-08 07:44:15 -0700

    Restructure The Website

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
-import headerData from "./header_data.jsx";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import { CgProfile } from "react-icons/cg";
 import { GoSignOut } from "react-icons/go";
 import { BiBrain } from "react-icons/bi"
+import { BsCart2 } from "react-icons/bs";
+import { AiOutlineHome, AiOutlineUserAdd, AiOutlineContacts } from "react-icons/ai";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { FiLogIn } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router.js";
-import { BsCart2 } from "react-icons/bs";
 
 const Header = () => {
     const [userId, setUserId] = useState({});
@@ -21,93 +23,97 @@ const Header = () => {
         setUserId(userId);
     }, []);
     return (
-        <header className="global-header bg-success">
-            <nav className="navbar navbar-expand-xl navbar-light">
-                <div className="container">
-                    <span className="navbar-brand fw-bold">Tavlorify Store</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <HiOutlineBars3 />
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-around" id="navbarNav">
-                        <ul className="navbar-nav align-items-center justify-content-center flex-grow-1">
-                            {headerData.navbarLinks.map((data, index) =>
-                                <li className="nav-item" key={index}>
-                                    <Link className="nav-link color-black" href={data.route}>
-                                        <span className={`icon me-2`}>{data.icon}</span>
-                                        {data.pageTitle}
+        // Start Global Header
+        <header className="global-header">
+            {/* Start Top Header */}
+            <div className="top-header pt-2 pb-2">
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container">
+                        <a className="navbar-brand" href="#">Tavlorify Store</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul className="navbar-nav mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" href="/">
+                                        <FiLogIn />
+                                        <span className="ms-2">Log In</span>
                                     </Link>
                                 </li>
-                            )}
-                            <li className="nav-item ai-services-item">
-                                <div className="nav-link color-black">
-                                    <span className="icon me-2">
-                                        <BiBrain />
-                                    </span>
-                                    AI Services
-                                </div>
-                                <ul className="ai-services-list">
-                                    {headerData.aiServicesData.map((ai_service_info, index) => (
-                                        <li className="ai-service-item p-3" key={index}>
-                                            <Link className="ai-service-link" href={ai_service_info.route}>
-                                                <span className="icon me-2">{ai_service_info.icon}</span>
-                                                {ai_service_info.pageTitle}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </li>
-                            <li className="nav-item ">
-                                <Link className="nav-link color-black" href="/cart">
-                                    <span className={`icon me-2`}>
+                                <li className="nav-item bg-dark auth-item">
+                                    <Link className="nav-link text-white" aria-current="page" href="/">
+                                        <AiOutlineUserAdd />
+                                        <span className="ms-2">Sign Up for Free</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item ms-3">
+                                    <Link className="nav-link" aria-current="page" href="/">
                                         <BsCart2 />
-                                    </span>
-                                    Cart
-                                </Link>
-                            </li>
-                            <li className="nav-item ">
-                                {userId && <Link className="nav-link color-black" href="/cart">
-                                    <span className={`icon me-2`}>
-                                        <BsCart2 />
-                                    </span>
-                                    Orders
-                                </Link>}
-                            </li>
-                        </ul>
-                        <ul className="navbar-nav align-items-center">
-                            <li className="nav-item profile-item">
-                                <div className="border profile-icon-item p-2 text-center">
-                                    <CgProfile className="profile-icon" />
-                                </div>
-                                <ul className="authentication-list">
-                                    {userId && <li className="auth-item p-3">
-                                        <Link className="auth-link" href="/profile">
-                                            <span className="icon me-2">
-                                                <CgProfile />
-                                            </span>
-                                            My Profile
-                                        </Link>
-                                    </li>}
-                                    {!userId && headerData.authenticationData.map((authInfo, index) => (
-                                        <li className="auth-item p-3" key={index}>
-                                            <Link className="auth-link" href={authInfo.route}>
-                                                <span className="icon me-2">{authInfo.icon}</span>
-                                                {authInfo.pageTitle}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                    {userId && <li className="auth-item p-3" onClick={signOut}>
-                                        <span className="icon me-2">
-                                            <GoSignOut />
-                                        </span>
-                                        Sign Out
-                                    </li>}
-                                </ul>
-                            </li>
-                        </ul>
+                                        <span className="ms-2">My Cart</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item ms-3">
+                                    <Link className="nav-link" aria-current="page" href="/">
+                                        <MdProductionQuantityLimits />
+                                        <span className="ms-2">My Orders</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" href="/">
+                                        <CgProfile />
+                                        <span className="ms-2">My Profile</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item ms-3 bg-danger auth-item">
+                                    <Link className="nav-link text-white" aria-current="page" href="/">
+                                        <GoSignOut />
+                                        <span className="ms-2">Sign Out</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
+                </nav>
+            </div>
+            {/* End Top Header */}
+            {/* Start Bottom Header */}
+            <div className="bottom-header pt-3 pb-3">
+                {/* Start Container From Bootstrap */}
+                <div className="container">
+                    <ul className="website-explain-list d-flex">
+                        <li className="link-item">
+                            <Link className="link text-dark" href='/'>
+                                <span className="ms-2">Home</span>
+                            </Link>
+                        </li>
+                        <li className="link-item">
+                            <Link className="link text-dark" href='/'>
+                                <span className="ms-2">Products</span>
+                            </Link>
+                        </li>
+                        <li className="link-item">
+                            <Link className="link text-dark" href='/'>
+                                <span className="ms-2">AI Services</span>
+                            </Link>
+                        </li>
+                        <li className="link-item">
+                            <Link className="link text-dark" href='/'>
+                                <span className="ms-2">Who Are We ?</span>
+                            </Link>
+                        </li>
+                        <li className="link-item">
+                            <Link className="link text-dark" href='/'>
+                                <span className="ms-2">Contact Us</span>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
+                {/* End Container From Bootstrap */}
+            </div>
+            {/* End Bottom Header */}
         </header>
+        // End Global Header
     );
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,35 +2,30 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import { useEffect, useState } from "react";
 import { IoSearchCircleSharp } from "react-icons/io5";
-import home_data from '../../public/data/home_data';
 import Axios from "axios";
 import Link from 'next/link';
+import backImg1 from "../../public/images/backgrounds/1.jpg";
+import backImg2 from "../../public/images/backgrounds/2.png";
+import backImg3 from "../../public/images/backgrounds/3.jpg";
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Home() {
-
-  const [infoBoxAppearedIndex, setInfoBoxAppearedIndex] = useState(1);
-
-  const [isShowMoreProducts, setIsShowMoreProducts] = useState(false);
-
-  const [isAppearedShowMoreProductsBtn, setIsAppearedShowMoreProductsBtn] = useState(true);
-
-  const [isWaitStatus, setIsWaitStatus] = useState(false);
-
   const [productsData, setProductsData] = useState([]);
-
   const [errorMsg, setErrorMsg] = useState("");
-
   useEffect(() => {
-    Axios.get(`${process.env.BASE_API_URL}/products/all-products`)
-      .then((res) => {
-        let result = res.data;
-        if (typeof result === "string") {
-          setErrorMsg(result);
-        } else {
-          setProductsData(result);
-        }
-      })
-      .catch((err) => setErrorMsg(err));
+    // Axios.get(`${process.env.BASE_API_URL}/products/all-products`)
+    //   .then((res) => {
+    //     let result = res.data;
+    //     if (typeof result === "string") {
+    //       setErrorMsg(result);
+    //     } else {
+    //       setProductsData(result);
+    //     }
+    //   })
+    //   .catch((err) => setErrorMsg(err));
+    let header = document.querySelector("#__next .global-header"),
+      introduction = document.querySelector(".home .introduction");
+    introduction.style.minHeight = `calc(100vh - ${header.clientHeight}px)`;
   }, []);
 
   return (
@@ -41,119 +36,39 @@ export default function Home() {
       <Header />
       {/* Start Introduction Section */}
       <section className="introduction">
-        {home_data.infoBoxData.map((data) => (
-          /* Start Info Box */
-          <div className="info-box"
-            key={data.id}
-            style={{
-              backgroundImage: `url(${data.backImgSrc})`,
-              opacity: infoBoxAppearedIndex == data.id ? 1 : 0,
-              transform: infoBoxAppearedIndex == data.id ? "scale(1.2)" : "scale(1)",
-            }}>
-            <div className="overlay text-white d-flex justify-content-center align-items-center"></div>
-          </div>
-          /* End Info Box */
-        ))}
+        {/* Start Carousel Component From Bootstrap */}
+        <Carousel indicators={false}>
+          {/* Start Carousel Item */}
+          <Carousel.Item>
+            <div className="overlay d-flex align-items-center justify-content-center">
+              <Carousel.Caption>
+                <h1>Welcome to the Tavlorify Store</h1>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          {/* End Carousel Item */}
+          {/* Start Carousel Item */}
+          <Carousel.Item>
+            <div className="overlay d-flex align-items-center justify-content-center">
+              <Carousel.Caption>
+                <h2>Welcome to the Tavlorify Store</h2>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          {/* End Carousel Item */}
+          {/* Start Carousel Item */}
+          <Carousel.Item>
+            <div className="overlay d-flex align-items-center justify-content-center">
+              <Carousel.Caption>
+                <h2>Welcome to the Tavlorify Store</h2>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          {/* End Carousel Item */}
+        </Carousel>
+        {/* End Carousel Component From Bootstrap */}
       </section>
       {/* End Introduction Section */}
-      <div className='main-content-box'>
-        {home_data.infoBoxData.map((data, index) => (
-          /* Start Main Content */
-          infoBoxAppearedIndex == data.id && <main
-            key={index}
-            className="p-3"
-          // style={{
-          //   transform: infoBoxAppearedIndex == data.id ? "rotateX(360deg) scale(0.8)" : ""
-          // }}
-          >
-            {data.content}
-          </main>
-          /* End Main Content */
-        ))}
-      </div>
-      {/* Start Most Popular Of Products Section */}
-      <section className="most-popular-products pt-3 pb-5">
-        {/* Start Custom Container */}
-        <div className="custom-container text-center">
-          <h4 className="section-name mb-4">Most Popular Of Products</h4>
-          {/* Start Grid System */}
-          <div className="row mb-3">
-            {productsData.map((productInfo, index) => (
-              /* Start Column */
-              index < 12 && <div className="col-md-2" key={index}>
-                <div className="product-box">
-                  <Link href={`/products/${productInfo.name}/${productInfo._id}`}>
-                    <img src={`${process.env.BASE_API_URL}/${productInfo.imageSrc}`} alt={productInfo.name} className="product-image mb-3 canvas-prints-image prints-image" />
-                  </Link>
-                  <h6 className="product-name">{productInfo.name}</h6>
-                  <h6 className="product-type">{productInfo.type}</h6>
-                </div>
-              </div>
-              /* End Column */
-            ))}
-          </div>
-          {/* End Grid System */}
-          {/* Start Grid System */}
-          <div className="row">
-            {productsData.map((productInfo, index) => (
-              /* Start Column */
-              productInfo.id > 12 && isShowMoreProducts && <div className="col-md-3" key={index}>
-                <div className="product-box">
-                  <Link href="/">
-                    <img src={productInfo.imageSrc} alt="Product Image" className="product-image mb-3" />
-                  </Link>
-                  <h6 className="product-name">Canvas</h6>
-                </div>
-              </div>
-              /* End Column */
-            ))}
-          </div>
-          {/* End Grid System */}
-        </div>
-        {/* End Custom Container */}
-      </section>
-      {/* End Most Popular Of Products Section */}
-      {/* Start Contact Us Section */}
-      <section className="contact-us pt-5 pb-5">
-        {/* Start Custom Container */}
-        <div className="custom-container text-center">
-          <h2 className="section-name mb-5">Contact Us</h2>
-          {/* Start Grid System */}
-          <div className="row">
-            {/* Start Column */}
-            <div className="col-md-6">
-              aa
-            </div>
-            {/* End Column */}
-            {/* Start Column */}
-            <div className="col-md-6">
-              {/* Start Contact Us Form */}
-              <form className="contact-us-form">
-                <input
-                  type="text"
-                  placeholder="Please Enter Your Full Name"
-                  className="form-control p-3 mb-4"
-                />
-                <input
-                  type="email"
-                  placeholder="Please Enter Your E-mail Address"
-                  className="form-control p-3 mb-4"
-                />
-                <textarea
-                  placeholder="How can we help you ?"
-                  className="form-control p-3 mb-4"
-                ></textarea>
-                <button type="submit" className="btn btn-success pt-3 pb-3 ps-4 pe-4 d-block w-100">Send Message</button>
-              </form>
-              {/* End Contact Us Form */}
-            </div>
-            {/* End Column */}
-          </div>
-          {/* End Grid System */}
-        </div>
-        {/* End Custom Container */}
-      </section>
-      {/* End Contact Us Section */}
-    </div >
+    </div>
   );
 }
