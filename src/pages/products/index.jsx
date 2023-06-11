a1088b975ebc4bc74599571f79408a163721c7c0
anas derk <anas.derk2020@gmail.com>
2023-06-11 05:41:45 -0700

    Continue With Design Products Page

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import Head from "next/head";
-import { BiSearch } from "react-icons/bi";
+import { BiBox, BiSearch } from "react-icons/bi";
 import { IoIosClose } from "react-icons/io";
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -9,18 +9,34 @@ import Axios from "axios";
 const Products = () => {
     const [productsData, setProductsData] = useState([]);
     const [errorMsg, setErrorMsg] = useState("");
-    useEffect(() => {
-        Axios.get(`${process.env.BASE_API_URL}/products/all-products`)
-            .then((res) => {
-                let result = res.data;
-                if (typeof result === "string") {
-                    setErrorMsg(result);
-                } else {
-                    setProductsData(result);
-                }
-            })
-            .catch((err) => setErrorMsg(err));
-    }, []);
+    const [inputsChecked, setInputsChecked] = useState([]);
+    const [productName, setProductName] = useState([]);
+    const handleInputsCheckedChange = (e) => {
+        const { value, checked } = e.target;
+        if (checked) {
+            setInputsChecked([...inputsChecked, value]);
+        } else {
+            setInputsChecked(inputsCheckedList =>
+                inputsCheckedList.filter((inputValue) => inputValue !== value)
+            );
+        }
+    }
+    const handleProductNameChange = (e) => {
+        const { value } = e.target;
+        setProductName(value);
+    }
+    // useEffect(() => {
+    //     Axios.get(`${process.env.BASE_API_URL}/products/all-products`)
+    //         .then((res) => {
+    //             let result = res.data;
+    //             if (typeof result === "string") {
+    //                 setErrorMsg(result);
+    //             } else {
+    //                 setProductsData(result);
+    //             }
+    //         })
+    //         .catch((err) => setErrorMsg(err));
+    // }, []);
     return (
         <div className="products">
             <Head>
@@ -34,6 +50,35 @@ const Products = () => {
                     {/* Start Column */}
                     <div className="col-md-3">
                         <h5 className="text-center">Product Category</h5>
+                        <hr />
+                        <form className="select-category-form">
+                            {/* Start Input Box */}
+                            <div className="input-box mb-3">
+                                <input
+                                    type="checkbox"
+                                    id="canvas-input"
+                                    className="me-2"
+                                    value="canvas-prints"
+                                    name="product-types"
+                                    onChange={handleInputsCheckedChange}
+                                />
+                                <label htmlFor="canvas-input">Canvas Prints</label>
+                            </div>
+                            {/* End Input Box */}
+                            {/* Start Input Box */}
+                            <div className="input-box">
+                                <input
+                                    type="checkbox"
+                                    id="framed-input"
+                                    className="me-2"
+                                    value="framed-prints"
+                                    name="product-types"
+                                    onChange={handleInputsCheckedChange}
+                                />
+                                <label htmlFor="framed-input">Framed Prints</label>
+                            </div>
+                            {/* End Input Box */}
+                        </form>
                     </div>
                     {/* End Column */}
                     {/* Start Column */}
@@ -50,7 +95,12 @@ const Products = () => {
                                 <form className="product-search-form">
                                     <div className="search-input-box">
                                         <BiSearch className="search-icon" />
-                                        <input type="text" className="form-control product-search-input p-2" />
+                                        <input
+                                            type="text"
+                                            className="form-control product-search-input p-2"
+                                            placeholder="Please Enter Product Name"
+                                            onChange={handleProductNameChange}
+                                        />
                                         <IoIosClose className="reset-btn-icon" />
                                     </div>
                                 </form>
@@ -63,7 +113,7 @@ const Products = () => {
                         <section className="required-products">
                             {/* Start Grid System */}
                             <div className="row text-center">
-                                {productsData.map((productInfo, index) => (
+                                {productsData.length > 0 ? productsData.map((productInfo, index) => (
                                     /* Start Column */
                                     <div className="col-md-4" key={index}>
                                         <div className="product-box p-3">
@@ -80,7 +130,9 @@ const Products = () => {
                                         </div>
                                     </div>
                                     /* End Column */
-                                ))}
+                                )) : <div className="not-found-products-err-box d-flex flex-column align-items-center justify-content-center" >
+                                    <p className="alert alert-danger not-found-products-err">Sorry Not Found Any Products Now !</p>
+                                </div>}
                             </div>
                             {/* End Grid System */}
                         </section>
