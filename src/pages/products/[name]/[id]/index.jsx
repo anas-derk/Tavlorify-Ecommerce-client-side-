76742455ece25db709c016bdf6a78682bfe5a566
anas derk <anas.derk2020@gmail.com>
2023-06-13 05:12:51 -0700

    Redesign Text To Image Page

--- src/pages/products/[name]/[id]/index.jsx
+++ src/pages/products/[name]/[id]/index.jsx
@@ -9,7 +9,7 @@ const ProductInfo = ({ result }) => {
     const [quantity, setQuantity] = useState(1);
 
     const router = useRouter();
-    
+
     const addToCart = (e) => {
         e.preventDefault();
         console.log(result);
@@ -26,67 +26,70 @@ const ProductInfo = ({ result }) => {
             } else {
                 let canvasEcommerceProductsList = [];
                 canvasEcommerceProductsList.push(productInfoToCart);
-                localStorage.setItem("canvas-ecommerce-products", JSON.stringify(canvasEcommerceProductsList));                
+                localStorage.setItem("canvas-ecommerce-products", JSON.stringify(canvasEcommerceProductsList));
             }
         }
     }
 
     return (
         // Start Product Info Page
-        <div className="product-info bg-info">
+        <div className="product-info">
             <Head>
                 <title>Tavlorify Store - Product Info</title>
             </Head>
             <Header />
-            {/* Start Container From Bootstrap */}
-            <div className="container pt-4 pb-4">
-                <h1 className="text-center">Hello To You In Product Info Page</h1>
-                {typeof result === "object" ?
-                    /* Start Grid System */
-                    <div className="row align-items-center">
-                        {/* Start Column */}
-                        <div className="col-md-2">
-                            aaa
-                        </div>
-                        {/* End Column */}
-                        {/* Start Column */}
-                        <div className="col-md-5">
-                            <div className="product-image-box">
-                                <img src={`${process.env.BASE_API_URL}/${result.imageSrc}`} alt={result.name} className="product-image mb-3 canvas-prints-image prints-image" />
+            <div className="page-content">
+                <div className="overlay">
+                    {/* Start Container From Bootstrap */}
+                    <div className="container pt-4 pb-4">
+                        <h1 className="text-center welcome-msg mb-4">Hello To You In Product Info Page</h1>
+                        {typeof result === "object" ?
+                            /* Start Grid System */
+                            <div className="row align-items-center">
+                                {/* Start Column */}
+                                <div className="col-md-2">
+                                    aaa
+                                </div>
+                                {/* End Column */}
+                                {/* Start Column */}
+                                <div className="col-md-5">
+                                    <div className="product-image-box">
+                                        <img src={`${process.env.BASE_API_URL}/${result.imageSrc}`} alt={result.name} className="product-image mb-3 canvas-prints-image prints-image" />
+                                    </div>
+                                </div>
+                                {/* End Column */}
+                                {/* Start Column */}
+                                <div className="col-md-5">
+                                    {/* Start Product Details Box */}
+                                    <div className="product-details-box p-4">
+                                        <h3 className="text-center">Product Details:</h3>
+                                        <h5 className="mb-3">Product Name: {result.name}</h5>
+                                        <h5>Product Type: {result.type}</h5>
+                                        <h5>Product Dimentions: {result.dimentions}</h5>
+                                        <h5 className="mb-4">Product Price: {result.price} $</h5>
+                                        <form className="add-to-cart-form" onSubmit={addToCart}>
+                                            <input
+                                                type="number"
+                                                className="form-control mb-4 p-3"
+                                                placeholder="Please Enter Quantity"
+                                                min="1"
+                                                value={quantity}
+                                                onChange={(e) => setQuantity(e.target.value)}
+                                                required
+                                            />
+                                            <button type="submit" className="btn btn-success w-100 p-3">Add To Cart</button>
+                                        </form>
+                                    </div>
+                                    {/* End Product Details Box */}
+                                </div>
+                                {/* End Column */}
                             </div>
-                        </div>
-                        {/* End Column */}
-                        {/* Start Column */}
-                        <div className="col-md-5">
-                            {/* Start Product Details Box */}
-                            <div className="product-details-box bg-white border border-2 p-4 pb-5 pt-5">
-                                <h3 className="text-center">Product Details:</h3>
-                                <h5 className="mb-3">Product Name: {result.name}</h5>
-                                <h5>Product Type: {result.type}</h5>
-                                <h5>Product Dimentions: {result.dimentions}</h5>
-                                <h5 className="mb-4">Product Price: {result.price} $</h5>
-                                <form className="add-to-cart-form" onSubmit={addToCart}>
-                                    <input
-                                        type="number"
-                                        className="form-control mb-4"
-                                        placeholder="Please Enter Quantity"
-                                        min="1"
-                                        max="5"
-                                        value={quantity}
-                                        onChange={(e) => setQuantity(e.target.value)}
-                                        required
-                                    />
-                                    <button type="submit" className="btn btn-success w-100">Add To Cart</button>
-                                </form>
-                            </div>
-                            {/* End Product Details Box */}
-                        </div>
-                        {/* End Column */}
+                            /* End Grid System */
+                            : <p className="alert alert-danger">Sorry This Product It Not Found !!!</p>}
                     </div>
-                    /* End Grid System */
-                    : <p className="alert alert-danger">Sorry This Product It Not Found !!!</p>}
+                    {/* End Container From Bootstrap */}
+                </div>
             </div>
-            {/* End Container From Bootstrap */}
         </div >
         // End Product Info Page
     );
--- src/pages/text-to-image/index.jsx
+++ src/pages/text-to-image/index.jsx
@@ -41,6 +41,7 @@ const TextToImage = () => {
                 if (typeof result === "string") {
                     console.log(result);
                 } else {
+                    console.log(result)
                     setCategoriesData(result);
                 }
             })
@@ -100,155 +101,64 @@ const TextToImage = () => {
                 <title>Tavlorify Store - Text To Image</title>
             </Head>
             <Header />
-            {/* Start Custom Container */}
-            <div className="custom-container pt-5 pb-4">
-                <h3 className="text-center mb-5">Welcome To You In Text To Image AI Service</h3>
-                {/* Start Text To Image Box */}
-                <section className="text-to-image-box p-4 mb-4">
-                    <h5 className="text-center mb-4">Enter a Text Prompt</h5>
-                    <form
-                        className="generate-images-from-text-form text-center"
-                        onSubmit={textToImageGenerate}
-                    >
-                        <input
-                            type="text"
-                            placeholder="a dog riding a bicycle"
-                            className="form-control p-2 mb-2"
-                            required
-                            onChange={(e) => setTextPrompt(e.target.value)}
-                        />
-                        <button type="submit" className="btn search-btn">
-                            <IoSearchCircleSharp className="search-icon" />
-                        </button>
-                    </form>
-                    {/* Start Grid System */}
-                    <section className="row">
-                        {/* Start Column */}
-                        <div className="col-md-6">
-                            {/* Start Category */}
-                            <h6 className="mb-3">Category</h6>
-                            <div className="categories mb-4 d-flex flex-wrap">
-                                {categoriesData.map((category, index) => (
-                                    /* Start Category Box */
-                                    <div
-                                        className="category-box text-center"
-                                        key={index}
-                                        onClick={() => handleSelectCategory(index)}
+            {/* Start Page Content */}
+            <div className="page-content">
+                {/* Start Container */}
+                <div className="container pt-4 pb-4">
+                    <h1 className="text-center mb-5 welcome-msg pb-3">Welcome To You In Text To Image AI Service</h1>
+                    {/* Start Text To Image Box */}
+                    <section className="text-to-image-box p-4 mb-4">
+                        {/* Start Grid System */}
+                        <section className="row">
+                            {/* Start Column */}
+                            <div className="col-md-5">
+                                {/* Start Options Box */}
+                                <div className="options-box p-3 pt-2">
+                                    <h6 className="mb-2 text-center option-heading pb-2">Options Box</h6>
+                                    <h6 className="text-center mb-3 mt-3">Your Text Prompt</h6>
+                                    <form
+                                        className="generate-images-from-text-form text-center mb-4"
+                                        onSubmit={textToImageGenerate}
                                     >
-                                        <img
-                                            src={`${process.env.BASE_API_URL}/${category.imgSrc}`}
-                                            alt="Anime Img"
-                                            width="75"
-                                            height="75"
-                                            className="mb-2 category-img"
-                                            style={index === categorySelectedIndex ? { border: "2px solid #F00" } : {}}
+                                        <input
+                                            type="text"
+                                            placeholder="a dog riding a bicycle"
+                                            className="form-control mb-3"
+                                            required
+                                            onChange={(e) => setTextPrompt(e.target.value)}
                                         />
-                                        <span className="d-block">{category.name}</span>
-                                    </div>
-                                    /* End Category Box */
-                                ))}
-                            </div>
-                            {/* End Category */}
-                            <hr />
-                            {/* Start Styles */}
-                            {categorySelectedIndex > -1 && <h6 className="mb-2">Style</h6>}
-                            {categorySelectedIndex > -1 && categoryStyles.length > 0 && <section className="styles mb-4 d-flex flex-wrap">
-                                {categoryStyles.map((style, index) => (
-                                    /* Start Style Box */
-                                    <div
-                                        className="style-box text-center"
-                                        key={index}
-                                        onClick={() => handleSelectStyle(index)}
-                                    >
-                                        <img
-                                            src={`${process.env.BASE_API_URL}/${style.imgSrc}`}
-                                            alt="Anime Img"
-                                            width="75"
-                                            height="75"
-                                            className="mb-2 category-img"
-                                            style={index === styleSelectedIndex ? { border: "2px solid #F00" } : {}}
-                                        />
-                                        <span className="d-block">{style.name}</span>
-                                    </div>
-                                    /* End Style Box */
-                                ))}
-                            </section>}
-                            {/* End Styles */}
-                        </div>
-                        {/* End Column */}
-                        {/* Start Column */}
-                        <div className="col-md-6">
-                            {/* Start Generate Wait Box */}
-                            {isWaitStatus && <section className="generate-wait-box text-center p-3">
-                                <h6 className="wait-message mb-3">Generating The Required Images</h6>
-                                <span className="loader max-auto"></span>
-                            </section>}
-                            {/* End Generate Wait Box */}
-                            {errorMsg && <p className="alert alert-danger">{errorMsg}</p>}
-                            {/* Start Generated Images Box */}
-                            {generatedImageURLs.length > 0 && !isWaitStatus && <section className="generated-images-box">
-                                <h4 className="text-center mb-4">Generated Images</h4>
-                                {/* Start Grid System */}
-                                <div className="row">
-                                    {generatedImageURLs.map((url, index) => (
-                                        <Link href={{
-                                            pathname: "/available-products",
-                                            query: {
-                                                url: url,
-                                            },
-                                        }}
-                                            key={index}
-                                        >
-                                            <img src={url} alt="Image" className="created-image" />
-                                        </Link>
-                                    ))}
+                                        <h6 className="describe text-start">Describe what you want the AI to create .</h6>
+                                        {/* End Select Category Box */}
+                                        {/* <button type="submit" className="btn search-btn">
+                                            <IoSearchCircleSharp className="search-icon" />
+                                        </button> */}
+                                    </form>
+                                    <hr />
+                                    <h6 className="mb-3">Category</h6>
+                                    {/* Start Select Category Box */}
+                                    <section className="select-category-box p-3 d-flex align-items-center flex-row justify-content-center mb-4">
+                                        Art
+                                    </section>
+                                    {/* End Select Category Box */}
+                                    <hr />
+                                    <h6 className="mb-3">Style</h6>
+                                    {/* Start Select Style Box */}
+                                    <section className="select-category-box p-3 d-flex align-items-center flex-row justify-content-center">
+                                        Art
+                                    </section>
+                                    {/* End Select Style Box */}
                                 </div>
-                                {/* End Grid System */}
-                            </section>}
-                            {/* End Generated Images Box */}
-                        </div>
-                        {/* End Column */}
+                                {/* End Options Box */}
+                            </div>
+                            {/* End Column */}
+                        </section>
+                        {/* End Grid System */}
                     </section>
-                    {/* End Grid System */}
-                    {/* Start Select Image Type Section */}
-                    {styleSelectedIndex > -1 && <>
-                        <h6 className="mb-3">Image Type</h6>
-                        <select className="form-control w-50" onChange={(e) => {
-                            setGetImgDimentionsMsg("Waiting ...");
-                            setTimeout(() => {
-                                setImageType(e.target.value);
-                                setDimentions({});
-                                setGetImgDimentionsMsg("");
-                            }, 1000);
-                        }}>
-                            <option defaultValue="" hidden>Select Image Type</option>
-                            <option value="horizontal">Horizontal</option>
-                            <option value="vertical">Vertical</option>
-                            <option value="square">Square</option>
-                        </select>
-                    </>}
-                    {/* End Select Image Type Section */}
-                    <hr />
-                    {getImgDimentionsMsg && <p className="alert alert-warning">{getImgDimentionsMsg}</p>}
-                    {/* Start Select Image Dimensions Section */}
-                    {modelName && imageType && !getImgDimentionsMsg && <h6 className="mb-3">Image Dimensions</h6>}
-                    {modelName && imageType && !getImgDimentionsMsg && <select className="form-control w-50" onChange={(e) => {
-                        setDimentions({
-                            width: text_to_image_data.modelsDimentions[modelName][imageType][e.target.value].inPixel.width,
-                            height: text_to_image_data.modelsDimentions[modelName][imageType][e.target.value].inPixel.height,
-                        });
-                        setDimentionsIndex(parseInt(e.target.value));
-                    }}>
-                        <option defaultValue="" hidden>Select Image Dimensions</option>
-                        {text_to_image_data.modelsDimentions[modelName][imageType].map((dimensions, index) => (
-                            <option value={index} key={index}>{dimensions.inCm} cm</option>
-                        ))}
-                    </select>}
-                    {/* End Select Image Dimensions Section */}
-                </section>
-                {/* End Text To Image Box */}
+                    {/* End Text To Image Box */}
+                </div>
+                {/* End Container */}
             </div>
-            {/* End Custom Container */}
+            {/* End Page Content */}
         </div>
         // End Text To Image Page
     );
