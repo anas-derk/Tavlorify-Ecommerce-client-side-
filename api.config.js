61d952fa648a8ad7bdc2e1456a7a664a257ec862
anas derk <anas.derk2020@gmail.com>
2023-06-13 08:00:00 -0700

    Continue With Redesign Text To Image Page

---
+++ api.config.js
@@ -0,0 +1,13 @@
+const BASE_API_URL = "https://newapi.tavlorify.se";
+
+const requestOrigin = "https://tavlorify.se";
+
+const requestReceiver = "//newapi.tavlorify.se/(.*)";
+
+// const BASE_API_URL = "http://localhost:4000";
+
+// const requestOrigin = "http://localhost:3000";
+
+// const requestReceiver = "//localhost:4000/(.*)";
+
+module.exports = { BASE_API_URL, requestOrigin, requestReceiver };
