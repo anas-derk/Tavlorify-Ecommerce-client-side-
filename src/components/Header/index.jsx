0bd6e65229dee59d34ff2637118301062046e342
anas derk <anas.derk2020@gmail.com>
2023-07-27 18:28:43 -0700

    Start Regulate Bottom Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,6 +5,7 @@ import { BiBrain } from "react-icons/bi"
 import { BsCart2, BsInfoCircle } from "react-icons/bs";
 import { AiOutlineHome, AiOutlineUserAdd, AiOutlineContacts } from "react-icons/ai";
 import { MdProductionQuantityLimits } from "react-icons/md";
+import { HiArrowSmDown } from "react-icons/hi";
 import { FiLogIn } from "react-icons/fi";
 import { FaQuestion } from "react-icons/fa";
 import { useEffect, useState } from "react";
@@ -12,6 +13,7 @@ import { useRouter } from "next/router.js";
 
 const Header = () => {
     const [userId, setUserId] = useState({});
+    const [optionsLinkName, setOptionsLinkName] = useState("");
     const router = useRouter();
     const signOut = () => {
         localStorage.removeItem("e-commerce-canvas-user-id");
@@ -28,7 +30,7 @@ const Header = () => {
             <div className="top-header pt-2 pb-2">
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Tavlorify Store</a>
+                        <Link className="navbar-brand fw-bold" href="/">Tavlorify Store</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -85,24 +87,66 @@ const Header = () => {
             <div className="bottom-header pt-2 pb-2">
                 {/* Start Container From Bootstrap */}
                 <div className="container-fluid">
-                    <ul className="website-explain-list d-flex">
+                    <ul className="website-explain-list d-flex justify-content-center">
                         <li className="link-item p-2">
                             <Link className="link text-dark" href='/'>
                                 <AiOutlineHome />
                                 <span className="ms-2">Home</span>
                             </Link>
                         </li>
-                        <li className="link-item p-2">
+                        <li className="link-item p-2" style={optionsLinkName === "poster" ? { backgroundColor: "#CCC", borderRadius: "4px" } : {}}>
                             <Link className="link text-dark" href='/products'>
                                 <MdProductionQuantityLimits />
-                                <span className="ms-2">Products</span>
+                                <span className="ms-2">Poster</span>
                             </Link>
+                            <HiArrowSmDown
+                                className="show-and-hide-links-btn"
+                                onClick={() => setOptionsLinkName(value => value === "poster" ? "" : "poster")}
+                            />
+                            {optionsLinkName === "poster" && <ul className="links-box">
+                                <li className="p-3">
+                                    <Link href="/text-to-image" className="text-dark">
+                                        Text To Image
+                                    </Link>
+                                </li>
+                                <li className="p-3">
+                                    <Link href="/image-to-image" className="text-dark">
+                                        Image To Image
+                                    </Link>
+                                </li>
+                                <li className="p-3">
+                                    <Link href="/prints" className="text-dark">
+                                        Image To Image
+                                    </Link>
+                                </li>
+                            </ul>}
                         </li>
-                        <li className="link-item p-2">
+                        <li className="link-item p-2" style={optionsLinkName === "canvas" ? { backgroundColor: "#CCC", borderRadius: "4px" } : {}}>
                             <Link className="link text-dark" href='/text-to-image'>
                                 <BiBrain />
-                                <span className="ms-2">Text To Image</span>
+                                <span className="ms-2">Canvas</span>
                             </Link>
+                            <HiArrowSmDown
+                                className="show-and-hide-links-btn"
+                                onClick={() => setOptionsLinkName(value => value === "canvas" ? "" : "canvas")}
+                            />
+                            {optionsLinkName === "canvas" && <ul className="links-box">
+                                <li className="p-3">
+                                    <Link href="/text-to-image" className="text-dark">
+                                        Text To Image
+                                    </Link>
+                                </li>
+                                <li className="p-3">
+                                    <Link href="/image-to-image" className="text-dark">
+                                        Image To Image
+                                    </Link>
+                                </li>
+                                <li className="p-3">
+                                    <Link href="/prints" className="text-dark">
+                                        Image To Image
+                                    </Link>
+                                </li>
+                            </ul>}
                         </li>
                         <li className="link-item p-2">
                             <Link className="link text-dark" href='/image-to-image'>
