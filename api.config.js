1a26f7efbfbb73e746c8348f07df370af90b8b98
anas derk <anas.derk2020@gmail.com>
2023-07-29 11:35:08 -0700

    Change BASE API URL

--- api.config.js
+++ api.config.js
@@ -1,14 +1,14 @@
-// const BASE_API_URL = "https://newapi.tavlorify.se";
+const BASE_API_URL = "https://newapi.tavlorify.se";
 
-// const requestOrigin = "https://tavlorify.se";
+const requestOrigin = "https://tavlorify.se";
 
-// const requestReceiver = "//newapi.tavlorify.se/(.*)";
+const requestReceiver = "//newapi.tavlorify.se/(.*)";
 
-const BASE_API_URL = "http://localhost:4000";
+// const BASE_API_URL = "http://localhost:4000";
 
-const requestOrigin = "http://localhost:3000";
+// const requestOrigin = "http://localhost:3000";
 
-const requestReceiver = "//localhost:4000/(.*)";
+// const requestReceiver = "//localhost:4000/(.*)";
 
 module.exports = {
     BASE_API_URL,
