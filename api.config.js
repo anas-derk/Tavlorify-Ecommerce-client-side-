190c463aac981a39e0196e5bb049f5c36d7dddb8
anas derk <anas.derk2020@gmail.com>
2023-07-29 18:30:30 -0700

    Change API Config

--- api.config.js
+++ api.config.js
@@ -1,14 +1,14 @@
-const BASE_API_URL = "https://newapi.tavlorify.se";
-
-const requestOrigin = "https://tavlorify.se";
-
-const requestReceiver = "//newapi.tavlorify.se/(.*)";
-
-// const BASE_API_URL = "https://api1.outcircle2023.com";
+// const BASE_API_URL = "https://newapi.tavlorify.se";
 
 // const requestOrigin = "https://tavlorify.se";
 
-// const requestReceiver = "//api1.outcircle2023.com/(.*)";
+// const requestReceiver = "//newapi.tavlorify.se/(.*)";
+
+const BASE_API_URL = "https://api1.outcircle2023.com";
+
+const requestOrigin = "https://tavlorify.se";
+
+const requestReceiver = "//api1.outcircle2023.com/(.*)";
 
 // const BASE_API_URL = "http://localhost:5300";
 
