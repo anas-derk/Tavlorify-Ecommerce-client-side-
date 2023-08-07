12f2b601ba8360879031b1236930b2de8f6019d5
anas derk <anas.derk2020@gmail.com>
2023-08-06 17:02:49 -0700

    Change API Config For Deploy App

--- api.config.js
+++ api.config.js
@@ -4,17 +4,17 @@
 
 // const requestReceiver = "//newapi.tavlorify.se/(.*)";
 
-// const BASE_API_URL = "https://api1.outcircle2023.com";
+const BASE_API_URL = "https://api1.outcircle2023.com";
 
-// const requestOrigin = "https://tavlorify.se";
+const requestOrigin = "https://tavlorify.se";
 
-// const requestReceiver = "//api1.outcircle2023.com/(.*)";
+const requestReceiver = "//api1.outcircle2023.com/(.*)";
 
-const BASE_API_URL = "http://localhost:5300";
+// const BASE_API_URL = "http://localhost:5300";
 
-const requestOrigin = "http://localhost:3000";
+// const requestOrigin = "http://localhost:3000";
 
-const requestReceiver = "//localhost:5300/(.*)";
+// const requestReceiver = "//localhost:5300/(.*)";
 
 module.exports = {
     BASE_API_URL,
