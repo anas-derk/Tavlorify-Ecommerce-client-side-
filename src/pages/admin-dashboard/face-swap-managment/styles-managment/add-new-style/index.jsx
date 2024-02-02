b163870a01360799ae6587adfbaf4f4b552b7279
anas derk <anas.derk2020@gmail.com>
2024-02-02 15:39:49 -0800

    Start Programming Add New Category Style For Face Swap Page

--- src/pages/admin-dashboard/face-swap-managment/styles-managment/add-new-style/index.jsx
+++ src/pages/admin-dashboard/face-swap-managment/styles-managment/add-new-style/index.jsx
@@ -7,21 +7,17 @@ import { useRouter } from "next/router";
 
 export default function AddNewCategoryStyle() {
 
-    const [categoriesData, setCategoriesData] = useState([]);
+    const categoriesData = ["Man", "Pojke", "Kvinna", "Flicka"];
+
+    const styleImagesData = [
+        { formValidationKey: "verticalStyleImage", orientation: "Vertical"},
+        { formValidationKey: "horizontalStyleImage", orientation: "Horizontal" },
+        { formValidationKey: "squareStyleImage", orientation: "Square" },
+    ];
 
     const [categoryName, setCategoryName] = useState("");
 
-    const [styleName, setStyleName] = useState("");
-
-    const [stylePrompt, setStylePrompt] = useState("");
-
-    const [styleNegativePrompt, setStyleNegativePrompt] = useState("");
-
-    const [ddim_steps, setDdim_steps] = useState("");
-
-    const [strength, setStrength] = useState("");
-
-    const [styleImageFile, setStyleImageFile] = useState("");
+    const [styleImageFiles, setStyleImageFiles] = useState({ vertical: "", horizontal: "", square: "" });
 
     const [isAddingStatus, setIsAddingStatus] = useState(false);
 
@@ -37,103 +33,80 @@ export default function AddNewCategoryStyle() {
         const adminId = localStorage.getItem("tavlorify-store-admin-id");
         if (!adminId) {
             router.push("/admin-dashboard/login");
-        } else {
-            axios.get(`${process.env.BASE_API_URL}/image-to-image/categories/all-categories-data`)
-                .then((res) => {
-                    let result = res.data;
-                    if (typeof result === "string") {
-                        console.log(result);
-                    } else {
-                        setCategoriesData(result);
-                    }
-                })
-                .catch((err) => console.log(err));
         }
     }, []);
 
     const addNewCategoryStyle = async (e) => {
-        e.preventDefault();
-        setFormValidationErrors({});
-        let errorsObject = validations.inputValuesValidation([
-            {
-                name: "categoryName",
-                value: categoryName,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
+        try {
+            e.preventDefault();
+            setFormValidationErrors({});
+            let errorsObject = validations.inputValuesValidation([
+                {
+                    name: "categoryName",
+                    value: categoryName,
+                    rules: {
+                        isRequired: {
+                            msg: "Sorry, Can't Be Field Is Empty !!",
+                        },
                     },
                 },
-            },
-            {
-                name: "styleName",
-                value: styleName,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
+                {
+                    name: "verticalStyleImage",
+                    value: styleImageFiles.vertical,
+                    rules: {
+                        isRequired: {
+                            msg: "Sorry, Can't Be Field Is Empty !!",
+                        },
+                        isImage: {
+                            msg: "عذراً ، يجب أن يكون الملف أو الملفات صور من امتداد png أو jpg !!"
+                        },
                     },
                 },
-            },
-            {
-                name: "stylePrompt",
-                value: stylePrompt,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
+                {
+                    name: "verticalStyleImage",
+                    value: styleImageFiles.vertical,
+                    rules: {
+                        isRequired: {
+                            msg: "Sorry, Can't Be Field Is Empty !!",
+                        },
+                        isImage: {
+                            msg: "عذراً ، يجب أن يكون الملف أو الملفات صور من امتداد png أو jpg !!"
+                        },
                     },
                 },
-            },
-            {
-                name: "styleNegativePrompt",
-                value: styleNegativePrompt,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
+                {
+                    name: "horizontalStyleImage",
+                    value: styleImageFiles.horizontal,
+                    rules: {
+                        isRequired: {
+                            msg: "Sorry, Can't Be Field Is Empty !!",
+                        },
+                        isImage: {
+                            msg: "عذراً ، يجب أن يكون الملف أو الملفات صور من امتداد png أو jpg !!"
+                        },
                     },
                 },
-            },
-            {
-                name: "ddim_steps",
-                value: ddim_steps,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
+                {
+                    name: "squareStyleImage",
+                    value: styleImageFiles.square,
+                    rules: {
+                        isRequired: {
+                            msg: "Sorry, Can't Be Field Is Empty !!",
+                        },
+                        isImage: {
+                            msg: "عذراً ، يجب أن يكون الملف أو الملفات صور من امتداد png أو jpg !!"
+                        },
                     },
                 },
-            },
-            {
-                name: "strength",
-                value: strength,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
-                    },
-                },
-            },
-            {
-                name: "styleImageFile",
-                value: styleImageFile,
-                rules: {
-                    isRequired: {
-                        msg: "Sorry, Can't Be Field Is Empty !!",
-                    },
-                    isImage: {
-                        msg: "عذراً ، يجب أن يكون الملف أو الملفات صور من امتداد png أو jpg !!"
-                    },
-                },
-            },
-        ]);
-        setFormValidationErrors(errorsObject);
-        if (Object.keys(errorsObject).length == 0) {
-            let formData = new FormData();
-            formData.append("categoryName", categoryName);
-            formData.append("styleName", styleName);
-            formData.append("stylePrompt", stylePrompt);
-            formData.append("styleNegativePrompt", styleNegativePrompt);
-            formData.append("ddim_steps", ddim_steps);
-            formData.append("strength", strength);
-            formData.append("styleImgFile", styleImageFile);
-            setIsAddingStatus(true);
-            try {
+            ]);
+            setFormValidationErrors(errorsObject);
+            if (Object.keys(errorsObject).length == 0) {
+                let formData = new FormData();
+                formData.append("categoryName", categoryName);
+                for(let styleImageData of styleImagesData) {
+                    formData.append(styleImageData.formValidationKey, styleImageFiles[styleImageData.orientation]);
+                }
+                setIsAddingStatus(true);
                 const res = await axios.post(`${process.env.BASE_API_URL}/image-to-image/styles/add-new-style`, formData);
                 const result = await res.data;
                 if (result === "Adding New Category Style For Image To Image Page Process Is Succesfuly !!") {
@@ -145,79 +118,55 @@ export default function AddNewCategoryStyle() {
                     }, 2000);
                 }
             }
-            catch (err) {
-                console.log(err);
-                setIsErrorStatus(true);
-                let errorTimeout = setTimeout(() => {
-                    setIsErrorStatus(false);
-                    clearTimeout(errorTimeout);
-                }, 2000);
-            }
+        }
+        catch (err) {
+            setIsErrorStatus(true);
+            let errorTimeout = setTimeout(() => {
+                setIsErrorStatus(false);
+                clearTimeout(errorTimeout);
+            }, 2000);
         }
     }
 
     return (
         <div className="add-new-style">
             <Head>
-                <title>Tavlorify Store - Add New Category Style For Image To Image</title>
+                <title>Tavlorify Store - Add New Category Style For Face Swap</title>
             </Head>
             <ControlPanelHeader />
             <div className="content text-center pt-4 pb-4">
                 <div className="container-fluid">
-                    <h1 className="welcome-msg mb-4 fw-bold mx-auto pb-3">Hello To You In Add New Category Style Page For Image To Image</h1>
-                    <form className="add-new-category-form w-50 mx-auto mb-4" onSubmit={addNewCategoryStyle}>
+                    <h1 className="welcome-msg mb-4 fw-bold mx-auto pb-3">Hello To You In Add New Category Style Page For Face Swap</h1>
+                    <form className="add-new-style-form w-50 mx-auto mb-4" onSubmit={addNewCategoryStyle}>
                         <select
                             className={`form-control p-2 ${formValidationErrors["categoryName"] ? "border border-danger mb-2" : "mb-4"}`}
                             onChange={(e) => { setCategoryName(e.target.value.trim()); }}
                         >
                             <option defaultValue="" hidden>Select The Category</option>
                             {categoriesData.map((category, index) => (
-                                <option value={category.name} key={index}>{category.name}</option>
+                                <option value={index} key={index}>{category}</option>
                             ))}
                         </select>
                         {formValidationErrors["categoryName"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["categoryName"]}</p>}
-                        <input
-                            type="text"
-                            className={`form-control p-2 ${formValidationErrors["styleName"] ? "border border-danger mb-2" : "mb-4"}`}
-                            placeholder="Please Enter Style Name"
-                            onChange={(e) => setStyleName(e.target.value.trim())}
-                        />
-                        {formValidationErrors["styleName"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["styleName"]}</p>}
-                        <textarea
-                            style={{ resize: "none" }}
-                            className={`form-control p-2 ${formValidationErrors["stylePrompt"] ? "border border-danger mb-2" : "mb-4"}`}
-                            placeholder="Please Enter Style Prompt"
-                            onChange={(e) => setStylePrompt(e.target.value.trim())}
-                        ></textarea>
-                        {formValidationErrors["stylePrompt"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["stylePrompt"]}</p>}
-                        <textarea
-                            style={{ resize: "none" }}
-                            className={`form-control p-2 ${formValidationErrors["styleNegativePrompt"] ? "border border-danger mb-2" : "mb-4"}`}
-                            placeholder="Please Enter Style Negative Prompt"
-                            onChange={(e) => setStyleNegativePrompt(e.target.value.trim())}
-                        ></textarea>
-                        {formValidationErrors["styleNegativePrompt"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["styleNegativePrompt"]}</p>}
-                        <input
-                            type="text"
-                            className={`form-control p-2 ${formValidationErrors["ddim_steps"] ? "border border-danger mb-2" : "mb-4"}`}
-                            placeholder="Please Enter The Ddim Steps"
-                            onChange={(e) => setDdim_steps(e.target.value.trim())}
-                        />
-                        {formValidationErrors["ddim_steps"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["ddim_steps"]}</p>}
-                        <input
-                            type="text"
-                            className={`form-control p-2 ${formValidationErrors["strength"] ? "border border-danger mb-2" : "mb-4"}`}
-                            placeholder="Please Enter The Strength"
-                            onChange={(e) => setStrength(e.target.value.trim())}
-                        />
-                        {formValidationErrors["strength"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["strength"]}</p>}
-                        <input
-                            type="file"
-                            className={`form-control p-2 ${formValidationErrors["styleImageFile"] ? "border border-danger mb-2" : "mb-4"}`}
-                            placeholder="Please Enter Category Image"
-                            onChange={(e) => setStyleImageFile(e.target.files[0])}
-                        />
-                        {formValidationErrors["styleImageFile"] && <p className='error-msg text-danger mb-2'>{formValidationErrors["styleImageFile"]}</p>}
+                        {styleImagesData.map((styleImageData, index) => (
+                            <div className="style-image-data" key={index}>
+                                <h6 className="fw-bold mb-3">Please Select {styleImageData.orientation} Style Image</h6>
+                                <input
+                                    type="file"
+                                    className={`form-control p-2 ${formValidationErrors[styleImageData.formValidationKey] ? "border border-danger mb-2" : "mb-4"}`}
+                                    placeholder="Please Enter Category Image"
+                                    onChange={(e) => setStyleImageFiles(
+                                        {
+                                            ...styleImageFiles,
+                                            vertical: styleImageData.orientation === "Vertical" ? e.target.files[0] : "",
+                                            horizontal: styleImageData.orientation === "Horizontal" ? e.target.files[0] : "",
+                                            square: styleImageData.orientation === "Square" ? e.target.files[0] : "",
+                                        }
+                                    )}
+                                />
+                                {formValidationErrors[styleImageData.formValidationKey] && <p className='error-msg text-danger mb-2'>{formValidationErrors[styleImageData.formValidationKey]}</p>}
+                            </div>
+                        ))}
                         {!isAddingStatus && !isErrorStatus && !isSuccessStatus && <button type="submit" className="btn btn-success w-100 d-block mx-auto">Add Now</button>}
                         {isAddingStatus && <button type="submit" className="btn btn-warning w-100 d-block mx-auto" disabled>Adding Now ...</button>}
                         {isErrorStatus && <button type="submit" className="btn btn-danger w-100 d-block mx-auto" disabled>Sorry, Someting Went Wrong, Please Try Again</button>}
