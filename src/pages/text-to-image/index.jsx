92413c4d487f1bf74aa0aba24c02848123b5d5b8
anas derk <anas.derk2020@gmail.com>
2024-09-10 01:26:18 +0300

    Continue

--- src/pages/text-to-image/index.jsx
+++ src/pages/text-to-image/index.jsx
@@ -594,7 +594,7 @@ export default function TextToImage({
             });
             setIsWaitStatus(true);
             const result = (await axios.get(
-                `${process.env.BASE_API_URL}/generated-images/generate-image-using-text-to-image-service?textPrompt=${textPrompt}&prompt=${categoryStyles[styleSelectedIndex].prompt}&categoryName=${categoriesData[categorySelectedIndex].name}&styleName=${categoryStyles[styleSelectedIndex].name}&position=${imageType}&dimentionsInCm=${dimentionsInCm}&paintingType=${paintingType}&isExistWhiteBorder=${isExistWhiteBorderWithPoster}&frameColor=${frameColor}&model_name=${modelName}&negative_prompt=${categoryStyles[styleSelectedIndex].negative_prompt}&width=${dimentions.width}&height=${dimentions.height}
+                `${process.env.BASE_API_URL}/generated-images/generate-image-using-text-to-image-service?textPrompt=${textPrompt}&categoryName=${categoriesData[categorySelectedIndex].name}&styleName=${categoryStyles[styleSelectedIndex].name}&position=${imageType}&dimentionsInCm=${dimentionsInCm}&paintingType=${paintingType}&isExistWhiteBorder=${isExistWhiteBorderWithPoster}&frameColor=${frameColor}&width=${dimentions.width}&height=${dimentions.height}
             `)).data;
             const imageURL = `${process.env.BASE_API_URL}/${result.data}`;
             setTempImageType(imageType);
